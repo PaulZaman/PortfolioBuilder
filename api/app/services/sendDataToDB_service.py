f434a8fc461ba08a986ef7929b8746d989fd6e5a
CocaColux <marcoserhal@icloud.com>
2025-05-18 20:29:38 +0200

    Added tickers POST function for Firebase

---
+++ api/app/services/sendDataToDB_service.py
@@ -0,0 +1,47 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_KEY        = os.getenv('FIREBASE_API_KEY')
+PROJECT_ID     = os.getenv('FIREBASE_PROJECT_ID')
+collection     = 'Stocks'
+
+# URL de base SANS documentId
+BASE_URL = (
+    f"https://firestore.googleapis.com/v1/"
+    f"projects/{PROJECT_ID}/databases/(default)/documents/{collection}"
+)
+
+stocks = [
+    {'ticker': 'XOM', 'name': 'Exxon Mobil Corporation', 'sector': 'Energy'},
+    {'ticker': 'AAPL', 'name': 'Apple Inc.', 'sector': 'Technology'},
+    {'ticker': 'MSFT', 'name': 'Microsoft Corporation', 'sector': 'Technology'},
+    {'ticker': 'GOOGL', 'name': 'Alphabet Inc.', 'sector': 'Communication Services'},
+    {'ticker': 'AMZN', 'name': 'Amazon.com, Inc.', 'sector': 'Consumer Discretionary'},
+    {'ticker': 'TSLA', 'name': 'Tesla, Inc.', 'sector': 'Consumer Discretionary'},
+    {'ticker': 'JNJ',  'name': 'Johnson & Johnson', 'sector': 'Health Care'},
+    {'ticker': 'PG',   'name': "Procter & Gamble Co.",  'sector': "Consumer Staples"},
+    {'ticker': 'VZ',   'name': "Verizon Communications Inc.",  'sector': "Communication Services"},
+    {'ticker': 'WMT',  'name': "Walmart Inc.",  'sector': "Consumer Staples"},
+    {'ticker': 'TEVA', 'name': 'Teva Pharmaceutical Industries Limited', 'sector': 'Health Care'},
+
+]
+
+for stock in stocks:
+    payload = {
+        "fields": {
+            "ticker": {"stringValue": stock['ticker']},
+            "name":   {"stringValue": stock['name']},
+            "sector": {"stringValue": stock['sector']}
+        }
+    }
+
+    url = f"{BASE_URL}?key={API_KEY}&documentId={stock['ticker']}"
+    res = requests.post(url, json=payload)
+
+    if res.status_code == 200:
+        print(f"{stock['ticker']} added (ID: {stock['ticker']})")
+    else:
+        print(f"Error ({res.status_code}) for {stock['ticker']}: {res.text}")
